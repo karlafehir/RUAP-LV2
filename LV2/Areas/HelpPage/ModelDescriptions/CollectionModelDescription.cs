91d517a7b24de37e310de78621654c06d5ae90b8
karlafehir <fehirkarla@gmail.com>
2022-11-11 18:02:08 +0100

    Task 2

---
+++ LV2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LV2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
